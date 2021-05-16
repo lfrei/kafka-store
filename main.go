3c994e84cb3996e25111fdf0d934be1b3d138471
Lukas Frei <lukas.frei@zuehlke.com>
2021-05-16 13:53:17 +0200

    Start web service to request products

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func productHandler(w http.ResponseWriter, r *http.Request) {
+	// replace
+	w.Write([]byte("Iphone,Ipad"))
+}
+
+func startWebServer(endpoint string, port int) {
+	http.HandleFunc(endpoint, productHandler)
+	log.Fatal(http.ListenAndServe(fmt.Sprint(":", port), nil))
+}
 
 func main() {
-	fmt.Println("Kafka-Store with GO")
+	startWebServer("/products", 8080)
 }
