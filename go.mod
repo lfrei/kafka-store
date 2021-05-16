66b89cdf40350bc810cbd04c2f3caa834cbb25ba
Lukas Frei <lukas.frei@zuehlke.com>
2021-05-16 15:29:49 +0200

    Add go-cache dependency and getProduct endpoint

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/lfrei/kafka-store
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+)
--- main.go
+++ main.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/patrickmn/go-cache"
 	"log"
 	"net/http"
+	"time"
 )
+
+var products *cache.Cache
 
 func getProducts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"message": "get called"}`))
+}
+
+func getProduct(w http.ResponseWriter, r *http.Request) {
+	productId := mux.Vars(r)["id"]
+	product, found := products.Get(productId)
+	if found {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(product.(string)))
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
 
 func postProducts(w http.ResponseWriter, r *http.Request) {
@@ -18,13 +34,23 @@ func postProducts(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`{"message": "post called"}`))
 }
 
-func startWebServer(endpoint string) {
+func startWebServer() {
 	r := mux.NewRouter()
-	r.HandleFunc(endpoint, getProducts).Methods(http.MethodGet)
-	r.HandleFunc(endpoint, postProducts).Methods(http.MethodPost)
+	r.HandleFunc("/product/{id}", getProduct).Methods(http.MethodGet)
+	r.HandleFunc("/products", getProducts).Methods(http.MethodGet)
+	r.HandleFunc("/products", postProducts).Methods(http.MethodPost)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
+func setupCache() {
+	products = cache.New(5*time.Minute, 10*time.Minute)
+
+	//add some test data
+	products.Set("001", "IPhone", cache.NoExpiration)
+	products.Set("002", "IPad", cache.NoExpiration)
+}
+
 func main() {
-	startWebServer("/products")
+	setupCache()
+	startWebServer()
 }
