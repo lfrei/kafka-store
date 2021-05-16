ec955a4358cf5d23caba2c4a01b7b5dcc4fc39f1
Lukas Frei <lukas.frei@zuehlke.com>
2021-05-16 15:43:33 +0200

    Connect post/get product to cache

--- main.go
+++ main.go
@@ -3,18 +3,13 @@ package main
 import (
 	"github.com/gorilla/mux"
 	"github.com/patrickmn/go-cache"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
 )
 
 var products *cache.Cache
-
-func getProducts(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "get called"}`))
-}
 
 func getProduct(w http.ResponseWriter, r *http.Request) {
 	productId := mux.Vars(r)["id"]
@@ -29,25 +24,28 @@ func getProduct(w http.ResponseWriter, r *http.Request) {
 }
 
 func postProducts(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(`{"message": "post called"}`))
+	productId := mux.Vars(r)["id"]
+	product, err := ioutil.ReadAll(r.Body)
+	if err == nil {
+		products.Set(productId, string(product), cache.NoExpiration)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(`{"message": "product created"}`))
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+	}
 }
 
 func startWebServer() {
 	r := mux.NewRouter()
 	r.HandleFunc("/product/{id}", getProduct).Methods(http.MethodGet)
-	r.HandleFunc("/products", getProducts).Methods(http.MethodGet)
-	r.HandleFunc("/products", postProducts).Methods(http.MethodPost)
+	r.HandleFunc("/product/{id}", postProducts).Methods(http.MethodPost)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 func setupCache() {
 	products = cache.New(5*time.Minute, 10*time.Minute)
-
-	//add some test data
-	products.Set("001", "IPhone", cache.NoExpiration)
-	products.Set("002", "IPad", cache.NoExpiration)
 }
 
 func main() {
